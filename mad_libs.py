064f3f360d207b84ee4c2114ed9b6718519ca355
Melanie Gunn <melanie@melaniegunn.com>
2020-11-01 14:44:16 -0600

    updating the game

--- mad_libs.py
+++ mad_libs.py
@@ -1,21 +1,32 @@
-choice = int(input("Would you like mad libs 1,2,or 3?"))
-if choice == 1:
+
+
+mad_libs_1 = """Mad libs 1- Once there was a little {} who lived in the forest. The {} {} went so {} that they crashed through the cieling.  
+They hurt their head and had to find a nurse.  The end.\n"""
+
+mad_libs_2 = """Mad Libs 2- One day the {} {} went swimming. The {} {} participated in a swimming race.  
+The {} {} was so {} that they won the swimming race. The {} {} got a gold medal. The end.\n"""
+
+mad_libs_3 = """Mad Libs 3- The {} {} went over the river to {} their Grandma."""
+print(mad_libs_1)
+print(mad_libs_2)
+print(mad_libs_3)
+
+choice = input("Would you like:\n Mad Libs 1, \n Mad Libs 2, or \n Mad Libs 3? \n")
+if choice == "Mad Libs 1":
     verb = input("Type a past tense verb.")
     adjective = input("Type an adjective.")
     noun = input("Type a noun.")
-
-    print("Once there was a little {} who lived in the forest.".format(noun))
-    print("The little {} {} so {} that they crashed through the cieling.  They hurt their head and had to find a nurse.  The end.".format(noun, verb, adjective))
-
-elif choice == 2:
+    print(mad_libs_1.format(noun, adjective, noun, verb))
+elif choice == "Mad Libs 2":
     color = input("Type a color.")
     verb = input("Type a past tense verb.")
     adjective = input("Type an adjective.")
     noun = input("Choose boy or girl.")
-
-    print("One day the {} {} went swimming. The {} {} participated in a swimming race.".format(color, noun, color, noun))
-    print("The {} {} was so {} that they won the swimming race." .format(color, noun, adjective)) 
-    print("The {} {} got a gold medal. The end.".format(color, noun))
-    
+    feeling = input("Name a feeling.")
+    print(mad_libs_2.format(adjective, noun, adjective, noun, adjective, noun, feeling, adjective, noun))
 else:  
-    print("done")
+    adjective = input("Type an adjective.")
+    noun = input("Choose boy or girl.")
+    verb = input("Type a past tense verb.")
+    print(mad_libs_3 .format(adjective, noun, verb))
+
